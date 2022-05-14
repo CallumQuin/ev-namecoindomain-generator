d63bdcef5032b66e3e6f179667940b5a981444f8
Callum Quin <calllum.quin@gmail.com>
2022-05-15 10:35:36 +1200

    removed errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import AssetForm from './AssetForm';
 import EVAssets from "./EVAssets";
 import { Routes, Route } from "react-router-dom";
-import { useEffect } from 'react';
-import WebFont from 'webfontloader';
 
 
 function App() {
