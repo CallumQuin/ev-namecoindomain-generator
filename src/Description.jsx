98221677af004161a4f3d017dcbfd176d18415fa
Callum Quin <calllum.quin@gmail.com>
2022-05-15 18:10:12 +1200

    new desc

--- src/Description.jsx
+++ src/Description.jsx
@@ -22,15 +22,11 @@ const Description = ({ title, punycode, nmcAsset, registration,
 
 Mint: ${registration}
 
-The possession of this Emblem Vault (EV) built on the ERC-721 Ethereum token standard gives ownership to the Namecoin asset listed above and can be opened at any time for direct access to the underlying asset on its native chain.
+This vault contains a Namecoin Identity id/ asset. Launched in May 2012, Namecoin Identities refer to the first-ever attempt at a Web3 decentralized identity service, which aimed to trustlessly verify identity securely and without relying on a central party.
 
-This vault contains a Namecoin Identity id/ asset. Launched in May 2012, Namecoin Identities refer to the first-ever attempt at a Web3 decentralized identity service, which aimed to trustlessly verify identity securely and without relying on a central party storing or processing information. 
+There are a total of 7075 Namecoin IDs from 2012 to 2017, collectively they form the third ever NFT collection in the blockchain history after Historical Domains and Punycodes. They are also one of the first examples of utility NFTs, meaning that these non-fungible tokens (NFTs) have some purpose, apart from the art or aesthetic value.
 
-There are a total of 7075 Namecoin IDs from 2012 to 2017, collectively they form the third ever NFT collection in the blockchain history after Historical Domains and Punycodes. Namecoin Identities are also one of the first examples of utility NFTs, meaning that these non-fungible tokens (NFTs) have some purpose, apart from the art or aesthetic value.
-
-Namecoin is the first fork of Bitcoin and therefore the first altcoin. It’s a special purpose blockchain designed primarily for decentralized domain names and digital identity. The assets on the Namecoin blockchain are now agreed by the community to be among the earliest forms of NFTs. 
-
-All Namecoin assets expire after 36,000 blocks (250 days or ~8 months) after which they can be claimed by anyone. Therefore, it's necessary to keep a small amount of NMC (around 1-2) in this vault so it can be renewed automatically.`)
+Namecoin is the first fork of Bitcoin and therefore the first altcoin. It’s a special purpose blockchain designed primarily for decentralized domain names and digital identity. All Namecoin assets expire after 36,000 blocks (~8 months). Therefore, it's necessary to keep a small amount of NMC (around 1-2) in this vault so the asset can be renewed automatically.`)
     }
   }, [nmcAsset, punycode, registration])
 
